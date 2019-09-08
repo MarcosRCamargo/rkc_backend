16ef258256086fa076c37f6cef4b700aeb3c5caf
Marcos Rubens de Camargo <marcos.marrize@gmail.com>
2019-09-07 21:37:22 -0300

    Método de likes completo

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,10 +1,23 @@
 const Dev = require('../models/Dev');
 
 module.exports = {
-    store(req, res) {
-        console.log(req.params.devId);
-        console.log(req.headers.user);
+    async store(req, res) {
+        const { devId } = req.params;
+        const { user } = req.headers;
 
-        return res.json({ ok: true });
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if (!targetDev) {
+            return res.status(400).json({ error: 'Dev not exists' });
+        }
+        if (targetDev.likes.includes(loggedDev._id)) {
+            console.log('Deu Match');
+        }
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
+        return res.json(loggedDev);
     }
 };
